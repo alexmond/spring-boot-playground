15cb0af0c91ad1b7bb6e5b4069dc7b4af81e1adc
alexmond <alexmond@gmail.com>
2025-06-24 22:32:35 -0400

    Initial commit

---
+++ simple-boot-rest-sample/src/test/java/org/alexmond/simplebootrestsample/SimpleBootRestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alexmond.simplebootrestsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBootRestSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

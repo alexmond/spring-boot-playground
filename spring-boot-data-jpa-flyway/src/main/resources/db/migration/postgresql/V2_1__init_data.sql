b3073ded24e04f133962bb71db440e1ee3492b6b
krisz <kriszemails@gmail.com>
2025-09-06 14:29:54 -0400

    Add Flyway migrations for roles table creation and initial data population

---
+++ spring-boot-data-jpa-flyway/src/main/resources/db/migration/postgresql/V2_1__init_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles(id, role_id, role_name) VALUES (1, 1, 'ADMIN');
+INSERT INTO roles(id, role_id, role_name) VALUES (2, 2, 'SUPERUSER');
+INSERT INTO roles(id, role_id, role_name) VALUES (3, 3, 'USER');

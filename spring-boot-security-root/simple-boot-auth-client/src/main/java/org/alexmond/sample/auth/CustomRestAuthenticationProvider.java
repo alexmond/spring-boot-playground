e60b7071b60fa5d3d35658c845c463232b0b9159
alexmond <alexmond@gmail.com>
2025-09-27 22:24:12 -0400

    pom improvements

--- spring-boot-security-root/simple-boot-auth-client/src/main/java/org/alexmond/sample/auth/CustomRestAuthenticationProvider.java
+++ spring-boot-security-root/simple-boot-auth-client/src/main/java/org/alexmond/sample/auth/CustomRestAuthenticationProvider.java
@@ -1,13 +1,13 @@
 package org.alexmond.sample.auth;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-import org.springframework.http.MediaType;
 
 import java.util.Collections;
 
